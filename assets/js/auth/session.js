79f2484252f01c07b5a5eb363008db405474652c
mapuaJACL <jaclabao@mymail.mapua.edu.ph>
2025-03-16 21:12:43 +0800

    Updated for state change according to User role

--- assets/js/auth/session.js
+++ assets/js/auth/session.js
@@ -1,8 +1,20 @@
 import { supabase } from "../config/supabase.js";
 
-// Get current session function
-export function getCurrentSession() {
+// Get current session function with user role
+export async function getCurrentSession() {
     const session = supabase.auth.session();
+    if (session) {
+        const { data, error } = await supabase
+            .from('Users')
+            .select('role')
+            .eq('user_id', session.user.id)
+            .single();
+        if (error) {
+            console.error("Error fetching user role:", error.message);
+            return null;
+        }
+        session.user.role = data.role;
+    }
     return session;
 }
 
@@ -21,4 +33,13 @@ export function onSessionStateChange(callback) {
     supabase.auth.onAuthStateChange((event, session) => {
         callback(event, session);
     });
+}
+
+// Check if user is admin function
+export async function isAdmin() {
+    const session = await getCurrentSession();
+    if (session && (session.user.role === 'admin' || session.user.role === 'admin,barber')) {
+        return true;
+    }
+    return false;
 }
