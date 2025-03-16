82a5eea6268322cb7fc843ea7d0207cc4a7f48b9
mapuaJACL <jaclabao@mymail.mapua.edu.ph>
2025-03-16 16:07:38 +0800

    Update this for signup/login

--- assets/js/pages/signup/database.js
+++ assets/js/pages/signup/database.js
@@ -1,10 +1,6 @@
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
+import { signUp, login } from  "../../auth/auth.js";
 
-// Initialize Supabase client
-const supabaseUrl = "https://dcyaytjualtuwvantpek.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjeWF5dGp1YWx0dXd2YW50cGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMDU2NjksImV4cCI6MjA1NTc4MTY2OX0.RDsVndAUGJfQN90Ezg_xxoHF-506ST66e7cSzuQ26jM";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+// Message div
 function showMessage(message, divId) {
     var messageDiv = document.getElementById(divId);
 
